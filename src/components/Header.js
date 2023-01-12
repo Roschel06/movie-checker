c798808f824e627874da2e099e3885a7833d103c
Roschel06 <dev.pallmann@gmail.com>
2023-01-12 20:55:00 +0100

    work on new menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,66 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import Navbar from './Navbar';
+import { Link } from 'react-router-dom';
 import { BiCameraMovie } from 'react-icons/bi';
-import { RxHamburgerMenu, RxCross1 } from 'react-icons/rx';
-import { useState } from 'react';
-export default function Header() {
+import {useEffect, useState} from 'react'
+import { menuItems } from '../menuItems';
 
-    const [isNavExpanded, setIsNavExpanded] = useState(false)
+export default function Header2() {
+    
+    const [genreList, setGenreList] = useState()
+    
+    useEffect(()=>{
+        const getData = async () =>{
+            try{
+                const response = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
+                const data = await response.json()
+                setGenreList(data.genres)
+            } catch(error){
+                console.log(error.message);
+            }
+        }
+        getData()
+    }, [])
+    
+    console.log("🚀 ~ genreList", genreList)
+    
+    console.log("🚀 ~ menuItems", menuItems)
 
-  return (
-    <nav>
-        <div className="container">
+    let genreArray = []
+
+    const genreLinkList = async () => {
+        for ( let i of genreList){
+            genreArray.push(Object.values(i)[1])
+        }
+        console.log("🚀 ~ newArray", genreArray)
+    
+        const toObject= {...genreArray};
+    
+    console.log("The converted array to object is:", toObject);
+
+
+    /* NEXT STEP => add new key value pair for link path to objects  */
+
+/*     toObject.map((item, idx) => ({...item, bla: idx.name}))
+console.log("New 2:", toObject); */
+    
+/*  menuItems.push(...genreList)
+    console.log("🚀 ~ menuItems changed", menuItems)
+ */
+
+    }
+    genreLinkList()
+
+
+
+
+    return (
+        <header>
+            <div className="nav-area">
             <Link to='/' className='logo'>
                 <BiCameraMovie /><span>Movie Checker</span>
             </Link>
-            <button className="hamburger" onClick={() => {setIsNavExpanded(!isNavExpanded);}}>
-                {isNavExpanded ? <RxCross1 /> : <RxHamburgerMenu />}
-            </button>
-            <div
-                className={isNavExpanded ? "menu expanded" : "menu"}
-            >
-                <ul>
-                    <li>
-                        Category
-                    </li>
-                    <li>
-                        About
-                    </li>
-                </ul>
+            <Navbar />
             </div>
-        </div>
-    </nav>
-  )
-}
+        </header>
+    );
+};
---
+++ src/components/Header22.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import { BiCameraMovie } from 'react-icons/bi';
+import { RxHamburgerMenu, RxCross1 } from 'react-icons/rx';
+import {useEffect, useState} from 'react'
+export default function Header() {
+
+    const [isNavExpanded, setIsNavExpanded] = useState(false)
+    const [genreList, setGenreList] = useState()
+    
+    useEffect(()=>{
+        const getData = async () =>{
+            try{
+                const response = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
+                const data = await response.json()
+                setGenreList(data.genres)
+            } catch(error){
+                console.log(error.message);
+            }
+        }
+        getData()
+    }, [])
+    
+    console.log("🚀 ~ genreList", genreList)
+
+  return (
+    <nav>
+        <div className="container">
+            <Link to='/' className='logo'>
+                <BiCameraMovie /><span>Movie Checker</span>
+            </Link>
+            <button className="hamburger" onClick={() => {setIsNavExpanded(!isNavExpanded);}}>
+                {isNavExpanded ? <RxCross1 /> : <RxHamburgerMenu />}
+            </button>
+            <div className={isNavExpanded ? "menu expanded" : "menu"}>
+                <ul className='menu-main'>
+                    <li>
+                        Genres
+                        <ul className='menu-sub'>
+                            {genreList?.map((item, key) => <li key={key}><Link to='/'>{item.name}</Link></li>)}
+                        </ul>
+                    </li>
+                    <li>
+                        About
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav> 
+  )
+}
